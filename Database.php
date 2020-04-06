0a49f985e48c753263c69d94ac233f3be40e7bd8
Thomas Deniaux <thomas.deniaux@sts-sio-caen.info>
2020-04-06 16:39:24 +0200

    class Database

---
+++ Database.php
@@ -0,0 +1,15 @@
+<?php
+class Database
+{
+
+private $_nomBase;
+private $_utilisateur;
+private $_mdp;
+private $_nomServeur;
+
+public function connexion($_nomServeur, $_nomBase, $_utilisateur, $_mdp)
+{
+	
+
+}
+?>
