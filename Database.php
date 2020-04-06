b4a6480ce3bbcb541b739bdb7bf93af65581bedd
Thomas Deniaux <thomas.deniaux@sts-sio-caen.info>
2020-04-06 17:10:16 +0200

    class Database

--- Database.php
+++ Database.php
@@ -9,7 +9,15 @@ private $_nomServeur;
 
 public function connexion($_nomServeur, $_nomBase, $_utilisateur, $_mdp)
 {
-	
+	try
+{
+$connexion = new PDO(mysql:host="$_nomServeur";dbname="_nomBase", "$_utilisateur", "$_mdp");
+}
+catch (Exception $e)
+{
+	die('Erreur : ' . $e->getMessage());
+}
 
+}
 }
 ?>
